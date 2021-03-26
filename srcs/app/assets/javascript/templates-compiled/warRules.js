35dd0a3893de48282d05778774438e8da035b836
Pauline Gangloff <gangloff.pauline@gmail.com>
2021-03-26 18:04:59 +0100

    war schedule ok

---
+++ srcs/app/assets/javascript/templates-compiled/warRules.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['warRules'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"warRules declareWar\" id=\"warSchedule\">\n</div>";
+},"useData":true});
+})();
--- srcs/app/assets/javascript/views/guild/declareWarView.js
+++ srcs/app/assets/javascript/views/guild/declareWarView.js
@@ -2,7 +2,13 @@ import { Guild } from '../../models/guildModel'
 import { Wars } from '../../collections/warCollection'
 
 export const DeclareWar = Backbone.View.extend({
+  events: {
+    'click .nextWarSchedule': 'nextWarSchedule'
+  },
   initialize: function (options) {
+    this.templateWarSchedule = Handlebars.templates.warSchedule
+    this.templateWarRules = Handlebars.templates.warRules
+
     this.fromId = options.fromId
     this.onId = options.onId
     this.fromGuild = new Guild({ id: this.fromId })
@@ -24,19 +30,120 @@ export const DeclareWar = Backbone.View.extend({
   el: $('#app'),
 
   render: function () {
-    this.templateWarSchedule = Handlebars.templates.warSchedule
-
     this.fromWars.sort()
     this.onWars.sort()
 
     this.context.fromName = this.fromGuild.get('name')
     this.context.onName = this.onGuild.get('name')
+    this.context.onId = this.onId
 
     this.updateContextWarCalendar()
 
     const templateData = this.templateWarSchedule(this.context)
     this.$el.html(templateData)
     return this
+  },
+
+  areFill: function () {
+    const array = []
+    const input = document.getElementsByClassName('input')
+    let res = true
+    // if value is fill
+    for (let i = 0; i < input.length; i++) {
+      input[i].style.border = 'solid 1px #C4C4C4'
+      if (input[i].value === '') {
+        input[i].style.border = 'solid 2px var(--error-message-color)'
+        res = false
+      }
+    }
+    const fromDate = new Date()
+    let day = Number(document.getElementById('fromJJ').value)
+    let month = Number(document.getElementById('fromMM').value)
+    let year = Number(document.getElementById('fromYY').value)
+    fromDate.setFullYear(year, month - 1, day)
+    if (year.toString().length != 4) {
+      document.getElementById('fromYY').style.border = 'solid 2px var(--error-message-color)'
+      res = false
+    }
+    if (fromDate == 'Invalid Date' || fromDate < Date.now()) {
+      document.getElementById('fromYY').style.border = 'solid 2px var(--error-message-color)'
+      document.getElementById('fromMM').style.border = 'solid 2px var(--error-message-color)'
+      document.getElementById('fromJJ').style.border = 'solid 2px var(--error-message-color)'
+      res = false
+    }
+    day = Number(document.getElementById('toJJ').value)
+    month = Number(document.getElementById('toMM').value)
+    year = Number(document.getElementById('toYY').value)
+    const toDate = new Date()
+    toDate.setFullYear(year, month - 1, day)
+    if (year.toString().length != 4) {
+      document.getElementById('toYY').style.border = 'solid 2px var(--error-message-color)'
+      res = false
+    }
+    if (toDate == 'Invalid Date' || toDate < Date.now()) {
+      document.getElementById('toJJ').style.border = 'solid 2px var(--error-message-color)'
+      document.getElementById('toMM').style.border = 'solid 2px var(--error-message-color)'
+      document.getElementById('toYY').style.border = 'solid 2px var(--error-message-color)'
+      res = false
+    }
+
+    let hh = Number(document.getElementById('fromHH').value)
+    let mm = Number(document.getElementById('fromMIN').value)
+    if (!(hh >= 0 && hh <= 23)) {
+      document.getElementById('fromHH').style.border = 'solid 2px var(--error-message-color)'
+      res = false
+    }
+    if (!(mm >= 0 && mm <= 59)) {
+      document.getElementById('fromMIN').style.border = 'solid 2px var(--error-message-color)'
+      res = false
+    }
+    fromDate.setHours(hh, mm, 0)
+
+    hh = Number(document.getElementById('toHH').value)
+    mm = Number(document.getElementById('toMIN').value)
+    if (!(hh >= 0 && hh <= 23)) {
+      document.getElementById('toHH').style.border = 'solid 2px var(--error-message-color)'
+      res = false
+    }
+    if (!(mm >= 0 && mm <= 59)) {
+      document.getElementById('toMIN').style.border = 'solid 2px var(--error-message-color)'
+      res = false
+    }
+    toDate.setHours(hh, mm, 0)
+
+    array.push(fromDate)
+    array.push(toDate)
+    return array
+  },
+
+  nextWarSchedule: function () {
+    const date = this.areFill()
+    if (date) {
+      let res = true
+      if (date[0] >= date[1]) {
+        document.getElementById('error').style.display = 'flex'
+        document.getElementById('error').textContent = 'The dates are invalid'
+        res = false
+      }
+      for (let i = 0; i < this.fromWars.length; i++) {
+        if (new Date(this.fromWars.at(i).get('war_start')) <= date[1] && new Date(this.fromWars.at(i).get('war_end')) >= date[0]) {
+          document.getElementById('error').style.display = 'flex'
+          document.getElementById('error').textContent = this.fromGuild.get('name') + ' has already a war schedule to this date'
+          res = false
+        }
+      }
+      for (let i = 0; i < this.onWars.length; i++) {
+        if (new Date(this.onWars.at(i).get('war_start')) <= date[1] && new Date(this.onWars.at(i).get('war_end')) >= date[0]) {
+          document.getElementById('error').style.display = 'flex'
+          document.getElementById('error').textContent = this.onGuild.get('name') + ' has already a war schedule to this date'
+          res = false
+        }
+      }
+      if (res === true) {
+        const templateData = this.templateWarRules(this.context)
+        this.$el.html(templateData)
+      }
+    }
   },
 
   updateContextWarCalendar: function () {
