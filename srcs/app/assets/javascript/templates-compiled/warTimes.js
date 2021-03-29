0c4e707831cbf54a285eba1a31ead7761bd47979
Pauline Gangloff <gangloff.pauline@gmail.com>
2021-03-29 17:15:43 +0200

    delete try to add jquery schedule

--- srcs/app/assets/javascript/templates-compiled/warTimes.js
+++ srcs/app/assets/javascript/templates-compiled/warTimes.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['warSchedule'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+templates['warTimes'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
           return parent[propertyName];
@@ -8,12 +8,12 @@ templates['warSchedule'] = template({"compiler":[8,">= 4.3.0"],"main":function(c
         return undefined
     };
 
-  return "<div class=\"warSchedule declareWar\" id=\"warSchedule\">\n    <a class=\"close\" id=\"closeDeclareWar\" href='#guild/"
+  return "<div class=\"warRules declareWar\" id=\"warSchedule\">\n    <a class=\"close\" id=\"closeDeclareWar\" href='#guild/"
     + alias4(((helper = (helper = lookupProperty(helpers,"onId") || (depth0 != null ? lookupProperty(depth0,"onId") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"onId","hash":{},"data":data,"loc":{"start":{"line":2,"column":55},"end":{"line":2,"column":63}}}) : helper)))
     + "'>\n        <img src=\"./icons/esc.svg\">\n    </a>\n    <div class=\"title\">VS\n        <div class=\"guildsName from\">"
     + alias4(((helper = (helper = lookupProperty(helpers,"fromName") || (depth0 != null ? lookupProperty(depth0,"fromName") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"fromName","hash":{},"data":data,"loc":{"start":{"line":6,"column":37},"end":{"line":6,"column":49}}}) : helper)))
     + "</div>\n        <div class=\"guildsName on\">"
     + alias4(((helper = (helper = lookupProperty(helpers,"onName") || (depth0 != null ? lookupProperty(depth0,"onName") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"onName","hash":{},"data":data,"loc":{"start":{"line":7,"column":35},"end":{"line":7,"column":45}}}) : helper)))
-    + "</div>\n    </div>\n    <div class=\"warScheduleMiddle\">\n        <div class=\"declareMiddle warCalendarNext\">\n            <div class=\"declareWarTitle warScheduleTitle\">War schedule</div>\n            <div class=\"schedule-container\">\n                <img src=\"./icons/schedule-yellow.svg\" class=\"schedule\">\n            </div>\n            <div class=\"input-container-from-to\">\n                <div class=\"input-container from\">\n                    <input type=\"text\" class=\"datepicker\" id=\"datepicker-from\"></input>\n                </div>\n            </div>\n            <p id=\"error\" style=\"display: none;\"></p>\n            <div class=\"buttons\">\n                <button class=\"button next nextWarSchedule\">Next</button>\n            </div>\n        </div>\n    </div>\n\n</div>";
+    + "</div>\n    </div>\n    <div class=\"declareMiddle warRulesMiddle\">\n        <div class=\"declareWarTitle warRulesTitle\">War times</div>\n        <div class=\"container\">\n            <div id=\"scheduler\"></div>\n        </div>\n    </div>\n    <div class=\"buttons\">\n        <button class=\"button prev prevWarTimes\">Previous</button>\n        <button class=\"button next nextWarRules\">Validate</button>\n    </div>\n</div>";
 },"useData":true});
 })();
--- srcs/app/assets/javascript/views/guild/declareWarView.js
+++ srcs/app/assets/javascript/views/guild/declareWarView.js
@@ -3,12 +3,14 @@ import { Wars } from '../../collections/warCollection'
 
 export const DeclareWar = Backbone.View.extend({
   events: {
-    'click .nextWarSchedule': 'nextWarSchedule',
-    'click .prevWarRules': 'prevWarRules'
+    'click .nextWarRules': 'nextWarRules',
+    'click .prevWarTimes': 'prevWarTimes'
   },
   initialize: function (options) {
-    this.templateWarSchedule = Handlebars.templates.warSchedule
     this.templateWarRules = Handlebars.templates.warRules
+    this.templateWarTimes = Handlebars.templates.warTimes
+    this.startDate = undefined
+    this.endDate = undefined
 
     this.fromId = options.fromId
     this.onId = options.onId
@@ -37,8 +39,10 @@ export const DeclareWar = Backbone.View.extend({
     this.context.fromName = this.fromGuild.get('name')
     this.context.onName = this.onGuild.get('name')
     this.context.onId = this.onId
-
-    this.updateContextWarCalendar()
+    this.context.winReward = '1000'
+    this.context.maxUnanswered = '2'
+    this.context.ladder = 'checked'
+    this.context.tournaments = 'checked'
 
     const templateData = this.templateWarRules(this.context)
     this.$el.html(templateData)
@@ -55,7 +59,6 @@ export const DeclareWar = Backbone.View.extend({
       const endDate = new Date(this.fromWars.at(i).get('war_end'))
       // eslint-disable-next-line no-unmodified-loop-condition
       while (startDate <= endDate) {
-        // dates.push(startDate)
         dates.push(startDate.toLocaleDateString('fr', { year: 'numeric', month: '2-digit', day: '2-digit' }))
         startDate.setDate(startDate.getDate() + 1)
       }
@@ -65,208 +68,67 @@ export const DeclareWar = Backbone.View.extend({
       const endDate = new Date(this.onWars.at(i).get('war_end'))
       // eslint-disable-next-line no-unmodified-loop-condition
       while (startDate <= endDate) {
-        // dates.push(startDate)
         dates.push(startDate.toLocaleDateString('fr', { year: 'numeric', month: '2-digit', day: '2-digit' }))
         startDate.setDate(startDate.getDate() + 1)
       }
     }
 
+    const now = new Date()
+    while (1) {
+      if (dates.some(el => {
+        return el === now.toLocaleDateString('fr', { year: 'numeric', month: '2-digit', day: '2-digit' })
+      }) === true) {
+        now.setDate(now.getDate() + 1)
+      } else {
+        break
+      }
+    }
+
+    const endDate = new Date(now)
+    endDate.setHours(endDate.getHours() + 1)
+
     const $j = jQuery.noConflict()
-    $('#datepicker-from').daterangepicker({
-      // dateFormat: 'dd/mm/yyyy hh:mm A',
+    $('#daterangepicker').daterangepicker({
       minDate: new Date(),
       timePicker: true,
       locale: {
-        format: 'DD/M/YYYY hh:mm A'
+        format: 'DD/MM/YYYY hh:mm A'
       },
+      startDate: now,
+      endDate: endDate,
       isInvalidDate: function (date) {
         date = date.toDate().toLocaleDateString('fr', { year: 'numeric', month: '2-digit', day: '2-digit' })
         if (dates.some(el => el === date) === true) { return true }
         return false
       }
-
     })
   },
 
-  prevWarRules: function () {
-    const templateData = this.templateWarSchedule(this.context)
+  nextWarRules: function () {
+    this.startDate = $('#daterangepicker').data('daterangepicker').startDate
+    this.endDate = $('#daterangepicker').data('daterangepicker').endDate
+    this.context.winReward = document.getElementById('win-reward').value
+    this.context.maxUnanswered = document.getElementById('max-unanswered').value
+    if (document.getElementById('ladder').checked) {
+      this.context.ladder = 'checked'
+    } else {
+      this.context.ladder = undefined
+    }
+    if (document.getElementById('tournaments').checked) {
+      this.context.tournaments = 'checked'
+    } else {
+      this.context.tournaments = undefined
+    }
+
+    const templateData = this.templateWarTimes(this.context)
     this.$el.html(templateData)
+    const $j = jQuery.noConflict()
+    $('#scheduler').weekly_schedule()
   },
 
-  areFill: function () {
-    const array = []
-    const input = document.getElementsByClassName('input')
-    let res = true
-    // if value is fill
-    for (let i = 0; i < input.length; i++) {
-      input[i].style.border = 'solid 1px #C4C4C4'
-      if (input[i].value === '') {
-        input[i].style.border = 'solid 2px var(--error-message-color)'
-        res = false
-      }
-    }
-    const fromDate = new Date()
-    let day = Number(document.getElementById('fromJJ').value)
-    let month = Number(document.getElementById('fromMM').value)
-    let year = Number(document.getElementById('fromYY').value)
-    this.context.fromJJ = document.getElementById('fromJJ').value
-    this.context.fromMM = document.getElementById('fromMM').value
-    this.context.fromYY = document.getElementById('fromYY').value
-
-    fromDate.setFullYear(year, month - 1, day)
-    if (year.toString().length != 4) {
-      document.getElementById('fromYY').style.border = 'solid 2px var(--error-message-color)'
-      res = false
-    }
-    if (fromDate == 'Invalid Date' || fromDate < Date.now()) {
-      document.getElementById('fromYY').style.border = 'solid 2px var(--error-message-color)'
-      document.getElementById('fromMM').style.border = 'solid 2px var(--error-message-color)'
-      document.getElementById('fromJJ').style.border = 'solid 2px var(--error-message-color)'
-      res = false
-    }
-    day = Number(document.getElementById('toJJ').value)
-    month = Number(document.getElementById('toMM').value)
-    year = Number(document.getElementById('toYY').value)
-
-    this.context.toJJ = document.getElementById('toJJ').value
-    this.context.toMM = document.getElementById('toMM').value
-    this.context.toYY = document.getElementById('toYY').value
-
-    const toDate = new Date()
-    toDate.setFullYear(year, month - 1, day)
-    if (year.toString().length != 4) {
-      document.getElementById('toYY').style.border = 'solid 2px var(--error-message-color)'
-      res = false
-    }
-    if (toDate == 'Invalid Date' || toDate < Date.now()) {
-      document.getElementById('toJJ').style.border = 'solid 2px var(--error-message-color)'
-      document.getElementById('toMM').style.border = 'solid 2px var(--error-message-color)'
-      document.getElementById('toYY').style.border = 'solid 2px var(--error-message-color)'
-      res = false
-    }
-
-    let hh = Number(document.getElementById('fromHH').value)
-    let mm = Number(document.getElementById('fromMIN').value)
-
-    this.context.fromHH = document.getElementById('fromHH').value
-    this.context.fromMIN = document.getElementById('fromMIN').value
-
-    if (!(hh >= 0 && hh <= 23)) {
-      document.getElementById('fromHH').style.border = 'solid 2px var(--error-message-color)'
-      res = false
-    }
-    if (!(mm >= 0 && mm <= 59)) {
-      document.getElementById('fromMIN').style.border = 'solid 2px var(--error-message-color)'
-      res = false
-    }
-    fromDate.setHours(hh, mm, 0)
-
-    hh = Number(document.getElementById('toHH').value)
-    mm = Number(document.getElementById('toMIN').value)
-
-    this.context.toHH = document.getElementById('toHH').value
-    this.context.toMIN = document.getElementById('toMIN').value
-
-    if (!(hh >= 0 && hh <= 23)) {
-      document.getElementById('toHH').style.border = 'solid 2px var(--error-message-color)'
-      res = false
-    }
-    if (!(mm >= 0 && mm <= 59)) {
-      document.getElementById('toMIN').style.border = 'solid 2px var(--error-message-color)'
-      res = false
-    }
-    toDate.setHours(hh, mm, 0)
-
-    array.push(fromDate)
-    array.push(toDate)
-    return array
-  },
-
-  nextWarSchedule: function () {
-    const date = this.areFill()
-    if (date) {
-      let res = true
-      if (date[0] >= date[1]) {
-        document.getElementById('error').style.display = 'flex'
-        document.getElementById('error').textContent = 'The dates are invalid'
-        res = false
-      }
-      for (let i = 0; i < this.fromWars.length; i++) {
-        if (new Date(this.fromWars.at(i).get('war_start')) <= date[1] && new Date(this.fromWars.at(i).get('war_end')) >= date[0]) {
-          document.getElementById('error').style.display = 'flex'
-          document.getElementById('error').textContent = this.fromGuild.get('name') + ' has already a war scheduled at this date'
-          res = false
-        }
-      }
-      for (let i = 0; i < this.onWars.length; i++) {
-        if (new Date(this.onWars.at(i).get('war_start')) <= date[1] && new Date(this.onWars.at(i).get('war_end')) >= date[0]) {
-          document.getElementById('error').style.display = 'flex'
-          document.getElementById('error').textContent = this.onGuild.get('name') + ' has already a war scheduled at this date'
-          res = false
-        }
-      }
-      if (res === true) {
-        const templateData = this.templateWarRules(this.context)
-        this.$el.html(templateData)
-      }
-    }
-  },
-
-  updateContextWarCalendar: function () {
-    const now = new Date()
-    this.context.fromWars = []
-    for (let i = 0; i < this.fromWars.length; i++) {
-      const startDate = new Date(this.fromWars.at(i).get('war_start'))
-      const endDate = new Date(this.fromWars.at(i).get('war_end'))
-
-      if (startDate > now) {
-        this.context.fromWars.push(JSON.parse(JSON.stringify(this.fromWars.at(i))))
-        const length = this.context.fromWars.length - 1
-        this.context.fromWars[length].startDay = this.getDay(startDate.getDay())
-        this.context.fromWars[length].startDate = startDate.toLocaleDateString('fr', { year: 'numeric', month: '2-digit', day: '2-digit' })
-        this.context.fromWars[length].startHours = startDate.getHours()
-        this.context.fromWars[length].startMin = startDate.getMinutes()
-        this.context.fromWars[length].endDay = this.getDay(endDate.getDay())
-        this.context.fromWars[length].endDate = endDate.toLocaleDateString('fr', { year: 'numeric', month: '2-digit', day: '2-digit' })
-        this.context.fromWars[length].endHours = endDate.getHours()
-        this.context.fromWars[length].endMin = endDate.getMinutes()
-      }
-    }
-    this.context.onWars = []
-    for (let i = 0; i < this.onWars.length; i++) {
-      const startDate = new Date(this.onWars.at(i).get('war_start'))
-      const endDate = new Date(this.onWars.at(i).get('war_end'))
-      if (startDate > now) {
-        this.context.onWars.push(JSON.parse(JSON.stringify(this.onWars.at(i))))
-        const length = this.context.onWars.length - 1
-        this.context.onWars[length].startDay = this.getDay(startDate.getDay())
-        this.context.onWars[length].startDate = startDate.toLocaleDateString('fr', { year: 'numeric', month: '2-digit', day: '2-digit' })
-        this.context.onWars[length].startHours = startDate.getHours()
-        this.context.onWars[length].startMin = startDate.getMinutes()
-        this.context.onWars[length].endDay = this.getDay(endDate.getDay())
-        this.context.onWars[length].endDate = endDate.toLocaleDateString('fr', { year: 'numeric', month: '2-digit', day: '2-digit' })
-        this.context.onWars[length].endHours = endDate.getHours()
-        this.context.onWars[length].endMin = endDate.getMinutes()
-      }
-    }
-  },
-
-  getDay: function (day) {
-    switch (day) {
-      case 0:
-        return 'Sun'
-      case 1:
-        return 'Mon'
-      case 2:
-        return 'Tue'
-      case 3:
-        return 'Wed'
-      case 4:
-        return 'Thu'
-      case 5:
-        return 'Fri'
-      case 6:
-        return 'Sat'
-    }
+  prevWarTimes: function () {
+    const templateData = this.templateWarRules(this.context)
+    this.$el.html(templateData)
+    this.disableDates()
   }
 })
