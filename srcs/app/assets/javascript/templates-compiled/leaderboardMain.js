577b416b0adc11862b4a7b0b5110a86b9d82c621
Pauline Gangloff <gangloff.pauline@gmail.com>
2021-04-02 17:56:31 +0200

    fix css page leaderboard

--- srcs/app/assets/javascript/templates-compiled/leaderboardMain.js
+++ srcs/app/assets/javascript/templates-compiled/leaderboardMain.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['leaderboardMain'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"listsPage leaderboardPage\" id=\"leaderboardPage\">\n    <div class=\"listsHeader-container leaderboardHeader-container\" id=\"leaderboardHeader-container\"></div>\n    <div class=\"leaderboardList-container\" id=\"leaderboardList-container\"></div> \n</div>";
+    return "<div class=\"listsPage leaderboardPage\" id=\"leaderboardPage\">\n    <div class=\"listsHeader-container leaderboardHeader-container\" id=\"leaderboardHeader-container\"></div>\n    <div class=\"listsMain-container leaderboardList-container\" id=\"leaderboardList-container\"></div> \n</div>";
 },"useData":true});
 })();
