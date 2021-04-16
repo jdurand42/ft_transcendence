360e308c0c0abc9beefd34f16c3f95f8ce14d6ad
Pauline Gangloff <gangloff.pauline@gmail.com>
2021-04-16 19:59:57 +0200

    template compiled profileSubNavBar

--- srcs/app/assets/javascript/templates-compiled/profileSubNavBar.js
+++ srcs/app/assets/javascript/templates-compiled/profileSubNavBar.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['profileSubNavBar'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"subNavBar\">\n		<li id=\"matchHistory\"> <span class=\"subNavBarEl\">Matches</span></li>\n		<li id=\"achievements\"> <span class=\"subNavBarEl\">Achivements</span></li>\n		<li id=\"profileGuild\"> <span class=\"subNavBarEl\">Guild</span></li>\n		<li id=\"friends\"> <span class=\"subNavBarEl\">Friends</span></li>\n</div>\n";
+    return "<div class=\"subNavBar\">\n		<li id=\"matchHistory\"> <span class=\"subNavBarEl\">Matches</span></li>\n		<li id=\"achievements\"> <span class=\"subNavBarEl\">Achievements</span></li>\n		<li id=\"profileGuild\"> <span class=\"subNavBarEl\">Guild</span></li>\n		<li id=\"friends\"> <span class=\"subNavBarEl\">Friends</span></li>\n</div>\n";
 },"useData":true});
 })();
