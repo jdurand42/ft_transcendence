d9f0de949ad502fc7e71c144f0e0d0cc575843d2
Pauline Gangloff <gangloff.pauline@gmail.com>
2021-04-19 14:08:46 +0200

    subnavbar profile

--- srcs/app/assets/javascript/templates-compiled/profileSubNavBar.js
+++ srcs/app/assets/javascript/templates-compiled/profileSubNavBar.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['profileSubNavBar'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"subNavBar\">\n		<li id=\"matchHistory\"> <span class=\"subNavBarEl\">Matches</span></li>\n		<li id=\"achievements\"> <span class=\"subNavBarEl\">Achievements</span></li>\n		<li id=\"profileGuild\"> <span class=\"subNavBarEl\">Guild</span></li>\n		<li id=\"friends\"> <span class=\"subNavBarEl\">Friends</span></li>\n</div>\n";
+    return "<div class=\"subNavBar\">\n	    <div class=\"square\" id=\"square\"></div>\n		<div class=\"match-history-nav subNavBarEl\" id=\"matchHistory\">\n			MATCHES\n		</div>\n		<div class=\"achievements-nav\" id=\"achievements\">\n			ACHIEVEMENTS\n		</div>\n		<div class=\"profile-guild-nav\" id=\"profileGuild\">\n			GUILD\n		</div>\n		<div class=\"friends-nav\" id=\"friends\">\n			FRIENDS\n		</div>\n</div>\n";
 },"useData":true});
 })();
