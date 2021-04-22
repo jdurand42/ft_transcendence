77afe1a17ecbbff3a7d30b435b21852cfaeb0265
jukazekajou <jerome.d274@gmail.com>
2021-04-22 16:13:41 +0200

    yes

--- srcs/app/assets/javascript/services/websocket.js
+++ srcs/app/assets/javascript/services/websocket.js
@@ -41,7 +41,7 @@ export class MyWebSocket {
       if (msg.type === 'ping') {
         return
       }
-      console.log(msg)
+      // console.log(msg)
       if (msg.message) {
         try {
           objet.receiveMessage(msg)
