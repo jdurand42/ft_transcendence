21460e9a72bb9fefaa6eab763f3f05961537b8ae
Pauline Gangloff <gangloff.pauline@gmail.com>
2021-04-26 19:11:29 +0200

    choose a game page

--- srcs/app/assets/javascript/templates-compiled/home.js
+++ srcs/app/assets/javascript/templates-compiled/home.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['home'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"home\">\n    <div class=\"play-gif\" href=\"#play\"></div>\n</div>";
+    return "<div class=\"home\" id=\"home\">\n</div>";
 },"useData":true});
 })();
