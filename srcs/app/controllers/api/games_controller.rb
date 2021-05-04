9fcec0151bf158677e0e38bd1f86f76d6dcec68e
kh42z <tterrail@student.42.fr>
2021-05-04 13:54:44 +0200

    fix #199

--- srcs/app/controllers/api/games_controller.rb
+++ srcs/app/controllers/api/games_controller.rb
@@ -4,6 +4,7 @@ module Api
   class GamesController < ApiController
     before_action :set_game, only: %i[destroy]
     include(WarHelper)
+    include(GameOverHelper)
     include(CompetitionHelper)
 
     GameReducer = Rack::Reducer.new(
