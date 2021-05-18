4ab9db9283a962df3d3207e5c0a39390817cd03b
cbouleng <boulenge.charles28@gmail.com>
2021-05-18 18:03:07 +0200

    Chat: chat_invitation includes 'sender_id'

--- srcs/app/lib/chat_helper.rb
+++ srcs/app/lib/chat_helper.rb
@@ -7,7 +7,7 @@ module ChatHelper
     participants = participants[0, 1] if chat.privacy == 'direct_message'
     participants.each do |t|
       ChatParticipant.create(user_id: t, chat_id: chat.id)
-      ActionCable.server.broadcast("user_#{t}", { action: 'chat_invitation', id: chat.id })
+      ActionCable.server.broadcast("user_#{t}", { action: 'chat_invitation', sender_id: current_user.id, id: chat.id })
     end
   end
 end
